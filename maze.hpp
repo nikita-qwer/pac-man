125e3e1c01d2c654f5b27a69943af8f0705688ef
nikita-qwer <nik29.12.004@gmail.com>
2025-09-19 09:54:59 +0300

    Update maze.hpp

--- maze.hpp
+++ maze.hpp
@@ -1,1 +1,15 @@
+#pragma once
+#include "point.hpp"
+#include <vector>
+#include <string>
+
+class Maze {
+    std::vector<std::string> grid;
+
+public:
+    Maze();
+    const std::vector<std::string>& get() const { return grid; }
+};
+
+
 
