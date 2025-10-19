c2969b5cfb4223ed2892b266ae45a322b72d61a6
nikita-qwer <nik29.12.004@gmail.com>
2025-10-19 18:41:49 +0300

    Update pacman.hpp

--- pacman.hpp
+++ pacman.hpp
@@ -2,12 +2,22 @@
 #include "point.hpp"
 #include "direction.hpp"
 #include "maze.hpp"
+#include <iostream>
 
 class Pacman {
     Point pos;
 
 public:
-    Pacman();
+    Pacman(Point start = Point(1, 1));
+    Pacman(const Pacman& other);
+
     Point get() const;
     void move(Direction dir, const Maze& maze);
+
+    Pacman operator=(const Pacman& other);
+    bool operator==(const Pacman& other) const;
 };
+
+std::istream& operator>>(std::istream& in, Pacman& pacman);
+std::ostream& operator<<(std::ostream& out, const Pacman& pacman);
+
