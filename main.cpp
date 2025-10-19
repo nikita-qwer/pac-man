c8ad62ded41f8e967077af5f58732cc960d9cb07
nikita-qwer <nik29.12.004@gmail.com>
2025-10-19 18:46:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 #include "maze.hpp"
 #include "pacman.hpp"
 #include "ghost.hpp"
+#include "direction.hpp"
 
 int main() {
     Maze maze;
     Pacman pacman;
     Ghost ghost({5, 5});
-    
+
     std::cout << "Pac-Man Game Started!" << std::endl;
     maze.display();
-    
-    // misca Pac-Man la dreapta
-    pacman.move(Direction::RIGHT, maze);
-    
+
+    std::cout << "\nMoving Pacman to the Right..." << std::endl;
+    pacman.move(Direction::Right, maze);
+    std::cout << "Pacman position: " << pacman << std::endl;
+    std::cout << "Ghost position: " << ghost << std::endl;
+
     return 0;
 }
