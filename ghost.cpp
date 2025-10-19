bd462ad593e80ba3626235ed1560e100ca74228c
nikita-qwer <nik29.12.004@gmail.com>
2025-10-19 18:41:04 +0300

    Update ghost.cpp

--- ghost.cpp
+++ ghost.cpp
@@ -2,6 +2,33 @@
 
 Ghost::Ghost(Point p) : pos(p) {}
 
+Ghost::Ghost(const Ghost& other) : pos(other.pos) {}
+
 Point Ghost::get() const {
     return pos;
+}
+
+void Ghost::set(Point newPos) {
+    pos = newPos;
+}
+
+Ghost Ghost::operator=(const Ghost& other) {
+    pos = other.pos;
+    return *this;
+}
+
+bool Ghost::operator==(const Ghost& other) const {
+    return pos == other.pos;
+}
+
+std::istream& operator>>(std::istream& in, Ghost& ghost) {
+    Point p;
+    in >> p;
+    ghost = Ghost(p);
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Ghost& ghost) {
+    out << ghost.get();
+    return out;
 }
