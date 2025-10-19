4e15b79a6bd4b54fd9302bb69941603f095c48a0
nikita-qwer <nik29.12.004@gmail.com>
2025-10-19 18:40:24 +0300

    Update ghost.hpp

--- ghost.hpp
+++ ghost.hpp
@@ -1,11 +1,21 @@
 #pragma once
 #include "point.hpp"
+#include <iostream>
 
 class Ghost {
 private:
     Point pos;
 
 public:
-    Ghost(Point p);
+    Ghost(Point p = Point(0, 0));
+    Ghost(const Ghost& other);
+
     Point get() const;
+    void set(Point newPos);
+
+    Ghost operator=(const Ghost& other);
+    bool operator==(const Ghost& other) const;
 };
+
+std::istream& operator>>(std::istream& in, Ghost& ghost);
+std::ostream& operator<<(std::ostream& out, const Ghost& ghost);
