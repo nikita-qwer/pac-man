59edd63181bed40b54bb27aed7591ae9da535359
nikita-qwer <nik29.12.004@gmail.com>
2025-11-07 10:21:02 +0200

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -1,27 +1,32 @@
 #include "point.hpp"
+#include <tuple> // pentru std::tie
 
 Point::Point(int _x, int _y) : x(_x), y(_y) {}
 
-Point::Point(const Point& other) : x(other.x), y(other.y) {}
+Point::Point(const Point& other) = default;
 
-Point Point::operator=(const Point& other) {
-    x = other.x;
-    y = other.y;
+Point& Point::operator=(const Point& other) {
+    if (this != &other) {
+        x = other.x;
+        y = other.y;
+    }
     return *this;
 }
 
-bool Point::operator==(const Point& other) const {
-    return x == other.x && y == other.y;
+bool Point::operator==(const Point& other) const noexcept {
+    return std::tie(x, y) == std::tie(other.x, other.y);
+}
+
+bool Point::operator<(const Point& other) const noexcept {
+    return std::tie(x, y) < std::tie(other.x, other.y);
 }
 
 std::istream& operator>>(std::istream& in, Point& point) {
-    int x, y;
-    in >> x >> y;
-    point = Point(x, y);
+    in >> point.x >> point.y;
     return in;
 }
 
 std::ostream& operator<<(std::ostream& out, const Point& point) {
-    out << point.x << " " << point.y;
+    out << "(" << point.x << ", " << point.y << ")";
     return out;
 }
