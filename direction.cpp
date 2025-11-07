f3b8529ccbfafeae84e7b1749895cc2953aec6b0
nikita-qwer <nik29.12.004@gmail.com>
2025-11-07 10:08:59 +0200

    Update direction.cpp

--- direction.cpp
+++ direction.cpp
@@ -1,12 +1,25 @@
 #include "direction.hpp"
+#include <string>
+#include <iostream>
 
 std::ostream& operator<<(std::ostream& out, const Direction& direction) {
     switch (direction) {
-        case Direction::Top: out << "Top"; break;
-        case Direction::Left: out << "Left"; break;
-        case Direction::Right: out << "Right"; break;
-        case Direction::Bottom: out << "Bottom"; break;
-        default: break;
+        case Direction::Top:
+            out << "Top";
+            break;
+        case Direction::Left:
+            out << "Left";
+            break;
+        case Direction::Right:
+            out << "Right";
+            break;
+        case Direction::Bottom:
+            out << "Bottom";
+            break;
+        default:
+            out << "Unknown";
+            break;
     }
     return out;
 }
+
