8792e9e7fdfacdf97f98355ba49e18a1cb683685
nikita-qwer <nik29.12.004@gmail.com>
2025-11-07 10:13:55 +0200

    Update maze.hpp

--- maze.hpp
+++ maze.hpp
@@ -1,14 +1,16 @@
 #pragma once
-#include "point.hpp"
 #include <vector>
 #include <string>
+#include "point.hpp"
 
 class Maze {
+private:
     std::vector<std::string> grid;
 
 public:
     Maze();
+
     void display() const;
-    bool isWallAt(Point p) const;
-    const std::vector<std::string>& get() const { return grid; }
+    bool isWallAt(const Point& p) const;
+    bool isInside(const Point& p) const; // metodă utilă suplimentară
 };
